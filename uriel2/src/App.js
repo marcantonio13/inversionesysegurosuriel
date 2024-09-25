e9d07324a62f32dc7aa3900cd2f9e8bde5bc6d57
Marco Antonio Flores Avellaneda <mfloresad@microformas.com.mx>
2024-09-24 23:47:32 -0600

    Primera prueba1

---
+++ uriel2/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="title-container">
+      <h1 className='responsive-title'>Hola Soy Uriel</h1>
+    </div>
+  );
+}
+
+export default App;
